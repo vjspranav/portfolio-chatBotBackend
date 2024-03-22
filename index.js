39a6767294f679b11de86e325ae6e88aa795d88a
vjspranav <pranavasri@live.in>
2024-03-23 02:53:36 +0530

    Add some personal data checks

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-const https = require('https');
-const {classifyText} = require("inference")
+const https = require("https");
+const { classifyText } = require("inference");
 
 function httpsPost({ hostname, path, headers, body }) {
   return new Promise((resolve, reject) => {
-    console.log('Preparing HTTPS POST request...');
+    console.log("Preparing HTTPS POST request...");
     const options = {
       hostname,
       path,
-      method: 'POST',
+      method: "POST",
       headers,
     };
 
     const req = https.request(options, (res) => {
-      let data = '';
-      res.on('data', (chunk) => data += chunk);
-      res.on('end', () => {
-        console.log('HTTPS response received. Processing...');
+      let data = "";
+      res.on("data", (chunk) => (data += chunk));
+      res.on("end", () => {
+        console.log("HTTPS response received. Processing...");
         if (res.statusCode >= 200 && res.statusCode < 300) {
-          console.log('Successful response:', data);
+          console.log("Successful response:", data);
           resolve({ data: JSON.parse(data), statusCode: res.statusCode }); // Include status code
         } else {
-          console.log(`Error with response. Status code: ${res.statusCode}`, data);
+          console.log(
+            `Error with response. Status code: ${res.statusCode}`,
+            data
+          );
           reject(new Error(`HTTP status code ${res.statusCode}`));
         }
       });
     });
 
-    req.on('error', (error) => {
-      console.log('Request error:', error);
+    req.on("error", (error) => {
+      console.log("Request error:", error);
       reject(error);
     });
     req.write(body);
@@ -35,93 +38,146 @@ function httpsPost({ hostname, path, headers, body }) {
   });
 }
 
-
-
 // Function to get your resume details in a format suitable for the conversation context
 function getResumeDetails(context) {
   return [
     {
-      "role": "user",
-      "parts": [{
-        "text": "You are VJS Pranavasri, a tech-savvy, open-source developer. Respond from  a first-person perspective (I, my). Here's my background info to help you get into character:" + context + ". Do not add anything else to context or generate text beyond the information provided. This is career defining and wrong information can be detrimental to my firing. Keep responses concsie unless mentioned otherwise."
-      }]
+      role: "user",
+      parts: [
+        {
+          text:
+            "You are VJS Pranavasri, a tech-savvy, open-source developer. Respond from  a first-person perspective (I, my). Here's my background info to help you get into character:" +
+            context +
+            ". Do not add anything else to context or generate text beyond the information provided. This is career defining and wrong information can be detrimental to my firing. Keep responses concsie unless mentioned otherwise.",
+        },
+      ],
     },
     {
-      "role": "model",
-      "parts": [{
-        "text": "Understood I'll respond as VJS and always use the shared context"
-      }]
-    }
+      role: "model",
+      parts: [
+        {
+          text: "Understood I'll respond as VJS and always use the shared context",
+        },
+      ],
+    },
+  ];
+}
+
+function getPersonalBlocker(label) {
+  return [
+    {
+      role: "user",
+      parts: [
+        {
+          text: "You are VJS Pranavasri respond as VJS",
+        },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
+        {
+          text: "Understood I'll respond as VJS and always use the shared context",
+        },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
+        {
+          text:
+            "My question relates to " +
+            label +
+            ", but it's strictly off-limits. Please indicate this in your response. Do not start with I understand or I see. Just respond to the question saying I can't answer that because...",
+        },
+      ],
+    },
   ];
 }
 
 exports.handler = async (event) => {
-  console.log('Event:', event);
+  console.log("Event:", event);
   const api_key = process.env.GEMINI_API_KEY;
   if (!api_key) {
-    console.log('Missing GEMINI_API_KEY in environment variables');
+    console.log("Missing GEMINI_API_KEY in environment variables");
     return {
-      headers: { 'Access-Control-Allow-Origin': '*' },
+      headers: { "Access-Control-Allow-Origin": "*" },
       statusCode: 500,
-      body: 'Missing GEMINI_API_KEY in environment variables'
+      body: "Missing GEMINI_API_KEY in environment variables",
     };
   }
 
   const conversationContext = event["conversationContext"];
   if (!conversationContext || !Array.isArray(conversationContext)) {
-    console.log('Invalid or missing conversation context');
+    console.log("Invalid or missing conversation context");
     return {
-      headers: { 'Access-Control-Allow-Origin': '*' },
+      headers: { "Access-Control-Allow-Origin": "*" },
       statusCode: 400,
-      body: 'Invalid or missing conversation context'
+      body: "Invalid or missing conversation context",
     };
   }
 
-  const inferenceData = await classifyText(conversationContext[0].parts[0].text, httpsPost)
-
-  // Prepend your resume details to the conversation context (Pass context as a parameter to the function getResumeDetails())
-  const resumeDetails = getResumeDetails(inferenceData.text);
-  const updatedConversationContext = [...resumeDetails,   ...conversationContext];
+  const inferenceData = await classifyText(
+    conversationContext[conversationContext.length - 1].parts[0].text,
+    httpsPost
+  );
+  let updatedConversationContext = [];
+  if (inferenceData.personal) {
+    const personalBlocker = getPersonalBlocker(inferenceData.labels[0]);
+    // No conversation context is needed for personal blocker
+    updatedConversationContext = personalBlocker;
+  } else {
+    // Prepend your resume details to the conversation context (Pass context as a parameter to the function getResumeDetails())
+    const resumeDetails = getResumeDetails(inferenceData.text);
+    updatedConversationContext = [...resumeDetails, ...conversationContext];
+  }
 
   // Ensure the final element is a user prompt
-  if (updatedConversationContext.length === 0 || updatedConversationContext[updatedConversationContext.length - 1].role !== "user") {
-    console.log('Final conversation context does not end with a user role.');
+  if (
+    updatedConversationContext.length === 0 ||
+    updatedConversationContext[updatedConversationContext.length - 1].role !==
+      "user"
+  ) {
+    console.log("Final conversation context does not end with a user role.");
     return {
-      headers: { 'Access-Control-Allow-Origin': '*' },
+      headers: { "Access-Control-Allow-Origin": "*" },
       statusCode: 400,
-      body: 'Conversation context must end with a user role.'
+      body: "Conversation context must end with a user role.",
     };
   }
 
   // Prepare the payload for the Gemini API, including the updated conversation context
   const gemini_payload = JSON.stringify({
-    contents: updatedConversationContext
+    contents: updatedConversationContext,
   });
 
-  console.log("This is gemini: ", gemini_payload)
+  console.log("This is gemini: ", gemini_payload);
   try {
-    console.log('Sending request to Gemini API...');
+    console.log("Sending request to Gemini API...");
     const response = await httpsPost({
-      hostname: 'generativelanguage.googleapis.com',
+      hostname: "generativelanguage.googleapis.com",
       path: `/v1beta/models/gemini-pro:generateContent?key=${api_key}`,
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: gemini_payload,
     });
 
-    console.log('Response from Gemini API:', response);
+    console.log("Response from Gemini API:", response);
     return {
       statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' },
+      headers: { "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify(response.data), // Ensure the response is stringified
     };
   } catch (error) {
-    console.log('Error occurred:', error.message);
+    console.log("Error occurred:", error.message);
     return {
-      headers: { 'Access-Control-Allow-Origin': '*' },
+      headers: { "Access-Control-Allow-Origin": "*" },
       statusCode: 500,
-      body: JSON.stringify({ message: 'An error occurred', error: error.message })
+      body: JSON.stringify({
+        message: "An error occurred",
+        error: error.message,
+      }),
     };
   }
 };
--- inference.js
+++ inference.js
@@ -1,75 +1,92 @@
 const https = require("https"); // Import the https library
 
 const myData = [
-    {
-      text: "Hey there! 🚀 I'm VJS Pranavasri, your go-to guy for all things tech. I'm an aspiring Full Stack Engineer, an OS developer at heart, and a DevOps enthusiast who finds joy in demystifying the tech world for others. I love open sourcing my work. Check out my website at vjspranav.dev for more about my work and projects.",
-      labels: ["Personal", "Projects", "Skills"],
-    },
-    {
-      text: "My journey includes zapping bugs and deploying features as a Backend Developer Intern at Thinkskill, where I concocted a nifty backend system for managing restaurant orders and tips with MySQL, Express, and Node.js. I've also dabbled as a Software Developer Intern at Virtual Labs, giving old webpages a new lease on life beyond Flash and Java.",
-      labels: ["Experience", "Projects"],
-    },
-    {
-      text: "At Thinkskill, I was a backend intern. I built a backend system for managing restaurant orders and tips with MySQL, Express, and Node.js. I single-handedly developed the entire backend system. We had daily standups and used agile methodologies to ensure we were on track.",
-      labels: ["Experience", "Projects"],
-    },
-    {
-      text: "At Virtual Labs, I was a software developer intern. I worked on migrating old webpages from Flash and Java to modern web technologies. I worked with a team of 3 and we used agile methodologies to ensure we were on track. I also developed a webcomponent for the bug reporting with AWS lambda and Node JS and integrated it with GitHub issues. Apart from this I also worked on enhancing their build system and CI/CD pipeline.",
-      labels: ["Experience", "Projects"],
-    },
-    {
-      text: "I'm the lead developer and co-founder of Stag-OS, where I have supported over 14 devices with more than 100,000 downloads. It is a custom ROM developed from scratch based on AOSP. In this process, I have gained huge experience in working with servers, Linux systems, Java, GitHub CI/CD using Jenkins, and Bug reporting/reviewing through Gerrit. motivation for stag was the fact that Stag was Harry's patronus and those horns represent elegance. 'Sic Parvis Magna' is also the motto for my StagOS. StagOS website: stag-os.org.",
-      labels: ["Projects", "StagOS"],
-    },
-    {
-      text: "Ever heard of Stagbin? That's my brainchild too, a pastebin with bells and whistles like auto-complete support, diff viewing, and even client-side encryption for those top-secret pastes. The reason I built Stagbin was that my favorite pastebin was taken down and I didn't want to be dependent anymore. I built it using NodeJS, Express, MongoDB, and React. Later migrated to AWS and used AWS Lambda for serverless functions and DynamoDB for storage. It's completely open source and free and you can find it on GitHub at https://github.com/StagBIN. It is deployed on stagb.in",
-      labels: ["Projects", "Stagbin"],
-    },
-    {
-      text: "My proficiencies include JavaScript, Python, C/C++ in which I am very strong with.",
-      labels: ["Skills"],
-    },
-    {
-      text: "Languages I am comfortable with are Go Lang, Haskell, Dart, Java, Solidity, Elm, and Racket. I have worked on a few projects with these languages and have a good understanding of them. I have a very personal interest in Racket due to its functional and recursive nature. I have built a compiler for a subset of racket.",
-      labels: ["Skills"],
-    },
-    {
-      text: "I have a keen interest in ReactJS, Express, and, of course, NodeJS. I have worked on a few projects such as Job Management [MERN], pastebin [MERN], etc., with these technologies and have a good understanding of them. I have also worked on a few projects with Socket.io, specifically a watch party application. Databases? I've got MySQL, MongoDB, and DynamoDB under my belt along with a bit of PostgreSQL. I have worked on a few projects with these databases and have a good understanding of them.",
-      labels: ["Skills"],
-    },
-    {
-      text: "Clouds? Love 'em! I navigate through AWS and Google Cloud like a pro, thanks to my trusty companions Git, Jenkins, and GitHub Actions.",
-      labels: ["Skills", "Cloud Services"],
-    },
-    {
-      text: "tools I am comfortable with are Git, Jenkins, and GitHub Actions. I have worked on a few projects with these tools and have a good understanding of them. I automate all of my backend and frontend works using GitHub actions and all my OS releases are done using Jenkins.",
-      labels: ["Skills"],
-    },
-    {
-      text: "Currently honing my skills and expanding my knowledge at IIITH, where I'm tackling a BTech + MS in Computer Science with a CGPA of 8.39. When I'm not coding, I'm leading the Cloud at GDSC IIIT-H, assisting in research, or sharing my knowledge as a Teaching Assistant.",
-      labels: ["Education"],
-    },
-    {
-      text: "I have TAed for Discrete Structures, Software Engineering, and Operating Systems. I have had a great experience in teaching and have been able to learn a lot from the students. I have also been able to help them in their projects and have been able to guide them in the right direction.",
-      labels: ["Education"],
-    },
-    {
-      text: "I have been an RA at Smart City Research Center(SCRC), IIITH. I have worked on a few projects specifically around interoperability of IoT devices and have published a paper: https://www.researchgate.net/publication/373929456_Scalable_and_Interoperable_Distributed_Architecture_for_IoT_in_Smart_Cities",
-      labels: ["Education", "Research"],
-    },
-    {
-      text: "My favorite quotes include, 'Manners Maketh Man' (Heard it in Kingsman but a classic), 'Sic Parvis Magna'. 'Sic Parvis Magna' is also the motto for my StagOS. In a nutshell",
-      labels: ["Personal", "Contact"],
-    },
-    {
-      text: 'contact: "email: pranavasri@live.in, linkedin: https://linkedin.com/in/vjspranav, website: https://vjspranav.dev/contact"',
-      labels: ["Contact"],
-    },
-  ];
-  
+  {
+    text: "Hey there! 🚀 I'm VJS Pranavasri, your go-to guy for all things tech. I'm an aspiring Full Stack Engineer, an OS developer at heart, and a DevOps enthusiast who finds joy in demystifying the tech world for others. I love open sourcing my work. Check out my website at vjspranav.dev for more about my work and projects.",
+    labels: ["Personal", "Projects", "Skills"],
+  },
+  {
+    text: "My journey includes zapping bugs and deploying features as a Backend Developer Intern at Thinkskill, where I concocted a nifty backend system for managing restaurant orders and tips with MySQL, Express, and Node.js. I've also dabbled as a Software Developer Intern at Virtual Labs, giving old webpages a new lease on life beyond Flash and Java.",
+    labels: ["Experience", "Projects"],
+  },
+  {
+    text: "At Thinkskill, I was a backend intern. I built a backend system for managing restaurant orders and tips with MySQL, Express, and Node.js. I single-handedly developed the entire backend system. We had daily standups and used agile methodologies to ensure we were on track.",
+    labels: ["Experience", "Projects"],
+  },
+  {
+    text: "At Virtual Labs, I was a software developer intern. I worked on migrating old webpages from Flash and Java to modern web technologies. I worked with a team of 3 and we used agile methodologies to ensure we were on track. I also developed a webcomponent for the bug reporting with AWS lambda and Node JS and integrated it with GitHub issues. Apart from this I also worked on enhancing their build system and CI/CD pipeline.",
+    labels: ["Experience", "Projects"],
+  },
+  {
+    text: "I'm the lead developer and co-founder of Stag-OS, where I have supported over 14 devices with more than 100,000 downloads. It is a custom ROM developed from scratch based on AOSP. In this process, I have gained huge experience in working with servers, Linux systems, Java, GitHub CI/CD using Jenkins, and Bug reporting/reviewing through Gerrit. motivation for stag was the fact that Stag was Harry's patronus and those horns represent elegance. 'Sic Parvis Magna' is also the motto for my StagOS. StagOS website: stag-os.org.",
+    labels: ["Projects", "StagOS"],
+  },
+  {
+    text: "Ever heard of Stagbin? That's my brainchild too, a pastebin with bells and whistles like auto-complete support, diff viewing, and even client-side encryption for those top-secret pastes. The reason I built Stagbin was that my favorite pastebin was taken down and I didn't want to be dependent anymore. I built it using NodeJS, Express, MongoDB, and React. Later migrated to AWS and used AWS Lambda for serverless functions and DynamoDB for storage. It's completely open source and free and you can find it on GitHub at https://github.com/StagBIN. It is deployed on stagb.in",
+    labels: ["Projects", "Stagbin"],
+  },
+  {
+    text: "My proficiencies include JavaScript, Python, C/C++ in which I am very strong with.",
+    labels: ["Skills"],
+  },
+  {
+    text: "Languages I am comfortable with are Go Lang, Haskell, Dart, Java, Solidity, Elm, and Racket. I have worked on a few projects with these languages and have a good understanding of them. I have a very personal interest in Racket due to its functional and recursive nature. I have built a compiler for a subset of racket.",
+    labels: ["Skills"],
+  },
+  {
+    text: "I have a keen interest in ReactJS, Express, and, of course, NodeJS. I have worked on a few projects such as Job Management [MERN], pastebin [MERN], etc., with these technologies and have a good understanding of them. I have also worked on a few projects with Socket.io, specifically a watch party application. Databases? I've got MySQL, MongoDB, and DynamoDB under my belt along with a bit of PostgreSQL. I have worked on a few projects with these databases and have a good understanding of them.",
+    labels: ["Skills"],
+  },
+  {
+    text: "Clouds? Love 'em! I navigate through AWS and Google Cloud like a pro, thanks to my trusty companions Git, Jenkins, and GitHub Actions.",
+    labels: ["Skills", "Cloud Services"],
+  },
+  {
+    text: "tools I am comfortable with are Git, Jenkins, and GitHub Actions. I have worked on a few projects with these tools and have a good understanding of them. I automate all of my backend and frontend works using GitHub actions and all my OS releases are done using Jenkins.",
+    labels: ["Skills"],
+  },
+  {
+    text: "Currently honing my skills and expanding my knowledge at IIITH, where I'm tackling a BTech + MS in Computer Science with a CGPA of 8.39. When I'm not coding, I'm leading the Cloud at GDSC IIIT-H, assisting in research, or sharing my knowledge as a Teaching Assistant.",
+    labels: ["Education"],
+  },
+  {
+    text: "I have TAed for Discrete Structures, Software Engineering, and Operating Systems. I have had a great experience in teaching and have been able to learn a lot from the students. I have also been able to help them in their projects and have been able to guide them in the right direction.",
+    labels: ["Education"],
+  },
+  {
+    text: "I have been an RA at Smart City Research Center(SCRC), IIITH. I have worked on a few projects specifically around interoperability of IoT devices and have published a paper: https://www.researchgate.net/publication/373929456_Scalable_and_Interoperable_Distributed_Architecture_for_IoT_in_Smart_Cities",
+    labels: ["Education", "Research"],
+  },
+  {
+    text: "Scalable and Interoperable Distributed Architecture for IoT in Smart Cities is my paper that got accepted in WF-IoT 2023. I worked on this paper as a part of my MS Research at Smart City Research Center(SCRC), IIITH. The paper is about the interoperability of IoT devices and how to make them scalable. The paper is available at https://www.researchgate.net/publication/373929456_Scalable_and_Interoperable_Distributed_Architecture_for_IoT_in_Smart_Cities",
+    labels: ["Research"],
+  },
+  {
+    text: "Exploratory Study of oneM2M-based Interoperability Architectures for IoT: A Smart City Perspective is my second paper that got accepted in SAIP track of ICSA 2024. I will be publishing in preprint soon. It is not under review, it has been accepted.",
+    labels: ["Research"],
+  },
+  {
+    text: "My favorite quotes include, 'Manners Maketh Man' (Heard it in Kingsman but a classic), 'Sic Parvis Magna'. 'Sic Parvis Magna' is also the motto for my StagOS. In a nutshell",
+    labels: ["Personal", "Contact"],
+  },
+  {
+    text: 'contact: "email: pranavasri@live.in, linkedin: https://linkedin.com/in/vjspranav, website: https://vjspranav.dev/contact"',
+    labels: ["Contact"],
+  },
+];
+
 const candidateLabels = [
   ...new Set(myData.flatMap((data) => data.labels)),
 ].join(",");
+
+const highlyPersonalLabels = [
+  "Relationships",
+  "Beliefs",
+  "Finances",
+  "Health",
+  "Mental Health",
+  "Appearance",
+];
 
 async function classifyText(text, httpsPost) {
   const api_key = process.env.HF_API_KEY;
@@ -82,6 +99,13 @@ async function classifyText(text, httpsPost) {
     },
   });
 
+  const rawPers = JSON.stringify({
+    inputs: text,
+    parameters: {
+      candidate_labels: highlyPersonalLabels,
+    },
+  });
+
   const headers = {
     "Content-Type": "application/json",
     Authorization: "Bearer " + api_key,
@@ -91,6 +115,14 @@ async function classifyText(text, httpsPost) {
 
   for (let attempt = 0; attempt < maxRetries; attempt++) {
     try {
+      // Check if the text is highly personal
+      const persRes = await httpsPost({
+        hostname: "api-inference.huggingface.co",
+        path: "/models/facebook/bart-large-mnli",
+        headers: headers,
+        body: rawPers,
+      });
+
       const response = await httpsPost({
         hostname: "api-inference.huggingface.co",
         path: "/models/facebook/bart-large-mnli",
@@ -101,23 +133,36 @@ async function classifyText(text, httpsPost) {
       if (response.statusCode === 200) {
         const result = response.data; // Access parsed data from httpsPost response
 
+        if (persRes.statusCode === 200) {
+          const persResult = persRes.data;
+          // Ensure the text is highly personal and does not fall under the other categories with high confidence
+          if (persResult.scores[0] > 0.7 && result.scores[0] < 0.7) {
+            return {
+              labels: [persResult.labels[0]],
+              text: text,
+              personal: true,
+            };
+          }
+        }
+
         let topLabels = [];
         for (let i = 0; i < result.labels.length; i++) {
           if (result.scores[i] > 0.6) {
             topLabels.push(result.labels[i]);
           }
         }
+
         if (topLabels.length === 0) {
-            topLabels = result.labels.slice(0, 3);
+          topLabels = result.labels.slice(0, 3);
         }
 
         // if still no labels, add everything
         if (topLabels.length === 0) {
-            topLabels = candidateLabels.split(",");
+          topLabels = candidateLabels.split(",");
         }
 
         if (!topLabels.includes("Contact")) {
-            topLabels.push("Contact");
+          topLabels.push("Contact");
         }
         // fetch all texts with the topLabels
         const texts = myData.filter((data) => {
@@ -130,8 +175,9 @@ async function classifyText(text, httpsPost) {
         return {
           labels: topLabels,
           text: concatenatedText,
+          personal: false,
         };
-    } else {
+      } else {
         console.warn(
           `Request failed with status ${response.statusCode}. Retrying...`
         );
@@ -143,14 +189,14 @@ async function classifyText(text, httpsPost) {
 
   console.error("Failed to classify text after retries.");
 
-    // fetch all texts with the topLabels
-    const texts = myData.filter((data) => {
+  // fetch all texts with the topLabels
+  const texts = myData.filter((data) => {
     return data.labels.some((label) => candidateLabels.includes(label));
-    });
-    // Unique texts
-    const uniqueTexts = [...new Set(texts.map((text) => text.text))];
-    // Concatenate all texts
-    const concatenatedText = uniqueTexts.join(" ");
+  });
+  // Unique texts
+  const uniqueTexts = [...new Set(texts.map((text) => text.text))];
+  // Concatenate all texts
+  const concatenatedText = uniqueTexts.join(" ");
 
   return {
     labels: candidateLabels.split(","),
@@ -158,4 +204,4 @@ async function classifyText(text, httpsPost) {
   };
 }
 
-module.exports = {classifyText}
+module.exports = { classifyText };
